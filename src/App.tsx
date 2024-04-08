0c51066db5e377df2dae39da5d3e2696fcef8fc7
lenayoo <lenayoo17@gmail.com>
2024-04-09 06:41:41 +0900

    faster

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [list, setList] = useState<string[]>([]);
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setTodo(value);
+  };
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setList([...list, todo]);
+    setTodo("");
+  };
+  console.log(list);
+
   return (
     <div className="flex justify-center items-center flex-col">
       <div className="header">
         <h1>Lena's Todo</h1>
       </div>
-      <div className="w-[350px] h-[500px] bg-indigo-400 rounded opacity-[75%] mt-10">
-        <form action="submit" className="mt-4">
-          <input type="text" className="w-48 rounded mr-4" />
+      <div className="w-[350px] h-[500px] bg-indigo-400 rounded opacity-[85%] mt-10">
+        <form action="submit" className="mt-4" onSubmit={submitHandler}>
+          <input
+            type="text"
+            className="w-48 rounded mr-4 py-1 px-3"
+            onChange={changeHandler}
+            value={todo}
+          />
           <button type="submit" className="">
             submit
           </button>
         </form>
+        <ul className="text-left pl-[20px] pt-4 text-lg">
+          {list.map((todo, index) => (
+            <li key={index}>
+              {index + 1} . {todo}
+            </li>
+          ))}
+        </ul>
       </div>
-      <div></div>
     </div>
   );
 };
