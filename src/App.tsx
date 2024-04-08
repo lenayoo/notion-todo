384209f4699c931ff4dd83c30f1800c1ea407b7f
lenayoo <lenayoo17@gmail.com>
2024-04-09 06:20:02 +0900

    finish css with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,14 @@ const App = () => {
       <div className="header">
         <h1>Lena's Todo</h1>
       </div>
-      <div className="w-96 h-96 bg-indigo-400 rounded opacity-1"></div>
+      <div className="w-[350px] h-[500px] bg-indigo-400 rounded opacity-[75%] mt-10">
+        <form action="submit" className="mt-4">
+          <input type="text" className="w-48 rounded mr-4" />
+          <button type="submit" className="">
+            submit
+          </button>
+        </form>
+      </div>
       <div></div>
     </div>
   );
